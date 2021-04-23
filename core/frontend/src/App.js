677252e7e773f419907fa8c12b6155872adbde62
ElUrquii <urquizamaxi099@gmail.com>
2021-04-23 09:42:27 -0300

    cambio a App.js

--- core/frontend/src/App.js
+++ core/frontend/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Esto es una App de React.
-        </p>
+        <p>Esto es una App de React.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
