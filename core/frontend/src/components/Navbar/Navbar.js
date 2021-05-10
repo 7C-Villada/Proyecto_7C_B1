33304e95798a1997f48429a91b9f2b69ff6ff27f
ElUrquii <urquizamaxi099@gmail.com>
2021-05-10 08:32:35 -0300

    Agregado componente Navbar + cambios de organización de arcgivos

---
+++ core/frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navb">
+        <h1 className="title">Esto es la navbar</h1>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
