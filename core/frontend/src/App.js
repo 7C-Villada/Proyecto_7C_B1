9003a1d6d56553d28fb4dccae0bfb0fa8e4290f1
ElUrquii <urquizamaxi099@gmail.com>
2021-05-11 11:47:55 -0300

    Navbar Completada

--- core/frontend/src/App.js
+++ core/frontend/src/App.js
@@ -1,11 +1,10 @@
+import React from "react";
 import Navbar from "./components/Navbar/Navbar";
-import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      <h1>Proyecto 7°C Burbuja 1</h1>
     </div>
   );
 }
--- core/frontend/src/components/Navbar/Navbar.js
+++ core/frontend/src/components/Navbar/Navbar.js
@@ -1,12 +1,56 @@
 import React from "react";
 import "./Navbar.css";
+import navLogo from "./Logo.png";
+import dropdownArrow from "./vectorpaint.svg";
 
 const Navbar = () => {
   return (
-    <div>
-      <nav className="navb">
-        <h1 className="title">Esto es la navbar</h1>
+    <div className="navb">
+      <img src={navLogo} className="logo" alt="nav Logo"></img>
+      <nav>
+        <ul className="nav__links">
+          <li>
+            <a href="/">Inicio</a>
+          </li>
+          <li>
+            <div className="dropdown">
+              <a href="7" className="dropdown-a">
+                Conocenos
+                <img
+                  src={dropdownArrow}
+                  className="dropdown-arrow"
+                  alt="arrow"
+                ></img>
+              </a>
+              <div className="dropdown__links">
+                <a href="/">Quienes Somos</a>
+                <a href="/">Que hacemos?</a>
+                <a href="/">Como lo hacemos?</a>
+              </div>
+            </div>
+          </li>
+          <li>
+            <a href="/">Talleres y Proyectos</a>
+          </li>
+          <li>
+            <div className="dropdown">
+              <a href="7" className="dropdown-a">
+                Campo Escuela
+                <img
+                  src={dropdownArrow}
+                  className="dropdown-arrow"
+                  alt="arrow"
+                ></img>
+              </a>
+              <div className="dropdown__links">
+                <a href="/">Reserva</a>
+                <a href="/">Campamentos</a>
+              </div>
+            </div>
+          </li>
+        </ul>
       </nav>
+      <button className="navButton">Colaborá</button>
     </div>
   );
 };
