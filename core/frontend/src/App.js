d1574f0cb8a1574101e33627fd4790d65317bfe3
ElUrquii <urquizamaxi099@gmail.com>
2021-05-13 19:10:11 -0300

    Agregadas views Prueba y Landing Page

--- core/frontend/src/App.js
+++ core/frontend/src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
+import Prueba from "./components/Prueba/Prueba";
+import LandingPage from "./components/LandingPage/LandingPage";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Switch>
+          <Route path="/prueba">
+            <Prueba />
+          </Route>
+          <Route path="/">
+            <LandingPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ core/frontend/src/components/Prueba/Prueba.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Prueba.css";
+
+const Prueba = () => {
+  return (
+    <div>
+      <h1>Prueba</h1>
+      <button className="botoncito">Botón de Prueba 1</button>
+      <button className="botoncito2">Botón de Prueba 2</button>
+      <div className="box"></div>
+      <p className="label1">TEXTO DE PRUEBA 1</p>
+      <p className="label2">TEXTO DE PRUEBA 2</p>
+    </div>
+  );
+};
+
+export default Prueba;
