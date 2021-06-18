88b1fec0b6410dc8cb1bada6f596d322e24c6603
ElUrquii <urquizamaxi099@gmail.com>
2021-06-18 16:53:39 -0300

    Seccion de Convenios

--- core/frontend/src/components/LandingPage/InfoSection.js
+++ core/frontend/src/components/LandingPage/InfoSection.js
@@ -3,12 +3,15 @@ import "./InfoSection.css";
 import CardSection from "./CardSection";
 import Footer from "../Footer/Footer";
 import MapSection from "./MapSection";
+import Convenios from "./Convenios";
+import { ConveniosData } from "./ConveniosData";
 
 const InfoSection = () => {
   return (
     <div className="info-section-container">
       <CardSection />
       <MapSection />
+      <Convenios slides={ConveniosData} />
       <Footer />
     </div>
   );
