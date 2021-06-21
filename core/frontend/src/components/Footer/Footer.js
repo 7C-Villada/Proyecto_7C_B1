5f1a19478b3964f3bee9c9b3e260e12c8240ff99
ElUrquii <urquizamaxi099@gmail.com>
2021-06-21 19:14:17 -0300

    Footer Section

--- core/frontend/src/components/Footer/Footer.js
+++ core/frontend/src/components/Footer/Footer.js
@@ -1,39 +1,36 @@
 import React from "react";
-import { FaInstagram } from "react-icons/fa";
-import { FaFacebookSquare } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaYoutube } from "react-icons/fa";
-import { FaEnvelope } from "react-icons/fa";
 import "./Footer.css";
 
-const Footer = () => {
+function Footer() {
   return (
-    <div className="footer-container">
-      <h1 className="footer-title">Seguinos en nuestras redes:</h1>
-      <ul className="footer-list">
-        <li>
-          <FaInstagram className="footer-icon" />
-          <h2 className="footer-info">fundacionaprenderhaciendo</h2>
-        </li>
-        <li>
-          <FaFacebookSquare className="footer-icon" />
-          <h2 className="footer-info">Fundación Aprender Haciendo</h2>
-        </li>
-        <li>
-          <FaLinkedin className="footer-icon" />
-          <h2 className="footer-info">Fundación Aprender Haciendo</h2>
-        </li>
-        <li>
-          <FaYoutube className="footer-icon" />
-          <h2 className="footer-info">Fundación Aprender Haciendo</h2>
-        </li>
-        <li>
-          <FaEnvelope className="footer-icon" />
-          <h2 className="footer-info">aprenderhaciendofund@gmail.com</h2>
-        </li>
-      </ul>
-    </div>
+      <div className="main-footer">
+        <div className="contenedor">
+          <div className="redessoc">
+            <h3 className="title" >Nuestras Redes Sociales:</h3>
+            <ui className="nombre">
+              <a href="https://www.instagram.com/fundacionaprenderhaciendo/?igshid=1b7iyx92lmbre">fundacionaprenderhaciendo</a>
+              <br/>
+              <a href="https://www.facebook.com/Fundaci%C3%B3n-Aprender-Haciendo-106219127385194/">Fundación Aprender Haciendo</a>
+              <br/>
+              <a href="https://ar.linkedin.com/in/fundaci%C3%B3n-aprender-haciendo-b4397b208">Fundación Aprender Haciendo</a>
+              <br/>
+              <a href="https://www.youtube.com/channel/UCZLlMVdcLU_UUwmOgTvr1MA">Fundación Aprender Haciendo</a>
+              <br/>
+              <a href="https://www.gmail.com">aprenderhaciendofund@gmail.com</a>
+            </ui>
+          </div>
+          <br />
+          <br />
+          <hr />
+          <div className="pie-footer">
+            <p className="info">
+              &copy;{new Date().getFullYear()} Copyright | Fundación Aprender Haciendo |
+              Diseñado por ITS Villada
+            </p>
+          </div>
+        </div>
+      </div>
   );
-};
+}
 
 export default Footer;
