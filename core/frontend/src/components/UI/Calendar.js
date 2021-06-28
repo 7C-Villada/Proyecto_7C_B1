f68101994a358fe27ef89c060778c5336d3766f0
ElUrquii <urquizamaxi099@gmail.com>
2021-06-28 17:11:40 -0300

    Agregado componente calendario sin estilo

--- core/frontend/src/components/UI/Calendar.js
+++ core/frontend/src/components/UI/Calendar.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { CalendarComponent } from "@syncfusion/ej2-react-calendars";
 import "./Calendar.css";
+import "../../App.css";
 
 const Calendar = () => {
   return (
-    <div className="calendar-container">
-      <h1>Calendario</h1>
-    </div>
+    // <div className="calendar-container">
+    //   <h1>Calendario</h1>
+    // </div>
+    <CalendarComponent id="calendar" className="calendar-container" />
   );
 };
 
