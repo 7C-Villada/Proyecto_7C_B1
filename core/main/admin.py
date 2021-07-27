8237bc487874249a1539d3d2c145035d76490e8c
FrankoH298 <francoh298@gmail.com>
2021-07-27 11:01:16 -0300

    Actualizado admin

--- core/main/admin.py
+++ core/main/admin.py
@@ -9,14 +9,38 @@ class ImageInline(admin.TabularInline):
 class ImageAlbumAdmin(admin.ModelAdmin):
     inlines = [ImageInline]
 
+@admin.register(Proyecto)
+class ProyectoAdmin(admin.ModelAdmin):
+    '''Admin View for Proyecto'''
+
+    list_display = ('title', 'description', 'startDate')
+    list_filter = ('startDate',)
+    search_fields = ('title', 'description', 'startDate')
+
+@admin.register(Taller)
+class TallerAdmin(admin.ModelAdmin):
+    '''Admin View for Taller'''
+
+    list_display = ('title', 'description', 'startDate')
+    list_filter = ('startDate',)
+    search_fields = ('title', 'description', 'startDate')
+
+@admin.register(Historia)
+class HistoriaAdmin(admin.ModelAdmin):
+    '''Admin View for Historia'''
+
+    search_fields = ('title', 'content')
+
+@admin.register(Convenio)
+class ConvenioAdmin(admin.ModelAdmin):
+    '''Admin View for Convenio'''
+
+    list_display = ('localizacion',)
+    search_fields = ('organizacion', 'localizacion')
 
 # Register your models here.
-admin.site.register(Proyecto)
-admin.site.register(Taller)
 admin.site.register(Image)
 admin.site.register(ImageAlbum, ImageAlbumAdmin)
-admin.site.register(Historia)
-admin.site.register(Convenio)
 admin.site.register(MercadoPagoLink)
 admin.site.register(MercadoLibreLink)
 admin.site.register(FormaParteLink)
