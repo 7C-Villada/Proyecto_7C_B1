b6e6cb64ada2f897d80b242be584375ba1069632
FrankoH298 <francoh298@gmail.com>
2021-08-24 11:28:13 -0300

    added boostrap, renamed folders, disabled old components, created new navbar

--- core/frontend/src/App.js
+++ core/frontend/src/App.js
@@ -1,18 +1,26 @@
+// React modules
 import React from "react";
-import Navbar from "./components/Navbar/Navbar";
-import Prueba from "./components/Prueba/Prueba";
-import Home from "./components/Pages";
-import Footer from "./components/Footer/Footer";
 import "./App.css";
 
+// Bootstrap modules
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
+
+// Primary components
+import Navbar from "./components/ui/Navbar/Navbar";
+// import Prueba from "./components/Prueba/Prueba";
+// import Home from "./components/Pages";
+// import Footer from "./components/Footer/Footer";
+
+// Secondary components
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Colabora from "./components/Pages/Colabora";
-import CampoEscuela from "./components/Pages/CampoEscuela";
-import Reserva from "./components/Pages/Reserva";
-import Campamentos from "./components/Pages/Campamentos";
-import Conocenos from "./components/Pages/Conocenos";
-import TalleresProyectos from "./components/Pages/TalleresProyectos";
-import NotFound from "./components/Pages/NotFound";
+// import Colabora from "./components/Pages/Colabora";
+// import CampoEscuela from "./components/Pages/CampoEscuela";
+// import Reserva from "./components/Pages/Reserva";
+// import Campamentos from "./components/Pages/Campamentos";
+// import Conocenos from "./components/Pages/Conocenos";
+// import TalleresProyectos from "./components/Pages/TalleresProyectos";
+// import NotFound from "./components/Pages/NotFound";
 
 function App() {
   return (
@@ -20,7 +28,7 @@ function App() {
       <div className="App">
         <Navbar />
         <Switch>
-          <Route path="/talleres-y-proyectos" component={TalleresProyectos} />
+          {/* <Route path="/talleres-y-proyectos" component={TalleresProyectos} />
           <Route path="/conocenos" component={Conocenos} />
           <Route path="/campamentos" component={Campamentos} />
           <Route path="/reserva" component={Reserva} />
@@ -28,7 +36,7 @@ function App() {
           <Route path="/colabora" component={Colabora} />
           <Route path="/prueba" component={Prueba} />
           <Route path="/" component={Home} exact />
-          <Route path="*" component={NotFound} />
+          <Route path="*" component={NotFound} /> */}
         </Switch>
         {/* <Footer /> */}
       </div>
---
+++ core/frontend/src/components/ui/Navbar/styles/Navbar.js
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+
+export const NavbarStyle = styled.nav.attrs({
+  className: "navbar navbar-expand-lg navbar-dark",
+})`
+  background: #3e2307;
+  height: 80px;
+  font-size: 1rem;
+`;
+
+export const NavButton = styled.button.attrs({
+  className: "btn btn-outline-light rounded border-0",
+})`
+  color: #f8f0bb !important;
+  &:hover {
+    background-color: #0fc11f;
+    color: #f8f0bb !important;
+  }
+  &:focus {
+    outline: none !important;
+    box-shadow: none;
+  }
+`;
+
+export const NavButtonDropdown = styled.button.attrs({
+  className: "btn btn-outline-light dropdown-toggle rounded border-0",
+})`
+  color: #f8f0bb !important;
+  &:hover {
+    background-color: #0fc11f !important ;
+    color: #f8f0bb !important;
+  }
+  &:focus {
+    outline: none !important;
+    box-shadow: none !important;
+  }
+`;
+
+export const UlDropdownMenu = styled.ul.attrs({
+  className: "dropdown-menu",
+})`
+  background: #f8f0bb !important;
+`;
+
+export const ButtonColabora = styled.button.attrs({
+  className: "btn btn-outline-light rounded-pill border-4",
+})`
+  color: #0fc11f;
+  border-color: #0fc11f;
+  $btn-padding-y: "10";
+  &:hover {
+    background-color: #0fc11f;
+    border-color: #0fc11f;
+  }
+  &:focus {
+    outline: none !important;
+    box-shadow: none;
+  }
+`;
