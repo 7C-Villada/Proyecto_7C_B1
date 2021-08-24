a912c927cc1beb2a05e6ae607640492caf496620
FrankoH298 <francoh298@gmail.com>
2021-08-24 12:04:10 -0300

    modified texts in hero

--- core/frontend/src/components/ui/Hero/Hero.js
+++ core/frontend/src/components/ui/Hero/Hero.js
@@ -4,13 +4,12 @@ const Hero = () => {
   return (
     <>
       <div className="px-4 py-5 my-5 text-center">
-        <h1 className="display-5 fw-bold">Centered hero</h1>
+        <h1 className="display-5 fw-bold">Aprender Haciendo</h1>
         <div className="col-lg-6 mx-auto">
           <p className="lead mb-4">
-            Quickly design and customize responsive mobile-first sites with
-            Bootstrap, the world’s most popular front-end open source toolkit,
-            featuring Sass variables and mixins, responsive grid system,
-            extensive prebuilt components, and powerful JavaScript plugins.
+            Nuestro objetivo principal es educar en la concientización del
+            ejercicio libre de los derechos de los niños y la conservación del
+            medio ambiente.
           </p>
           <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
             <button type="button" className="btn btn-primary btn-lg px-4 gap-3">
