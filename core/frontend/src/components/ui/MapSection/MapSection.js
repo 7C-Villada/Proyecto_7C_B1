c902b6bbb254471b6d3cb22188c29d8ebfc12bd0
BenjaCimatti <benja.cimatti@gmail.com>
2021-08-31 00:18:40 -0300

    Map added

--- core/frontend/src/components/ui/MapSection/MapSection.js
+++ core/frontend/src/components/ui/MapSection/MapSection.js
@@ -1,13 +1,25 @@
 import React from 'react'
-import { MapContainer, Header } from './style/MapSectionElements'
+import { Header } from './style/MapSectionElements'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const MapSection = () => {
+
     return (
         <>
             <div className='container py-5'>
                 <Header>Mapa Interactivo</Header>
                 <div className='row py-5'>
-                    <MapContainer />
+                    {/* <MapContainer /> */}
+                    <MapContainer
+                        center={[-31.1388288,-64.4520141]} 
+                        zoom={13} 
+                        style={{ width: '100%', height: '500px'}}
+                    >
+                        <TileLayer
+                            attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                    </MapContainer>
                 </div>
             </div>
         </>
