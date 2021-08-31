8caa9f8e37e249547d217e3eb38b069b668240e2
BenjaCimatti <benja.cimatti@gmail.com>
2021-08-31 11:26:47 -0300

    Fix Map

--- core/frontend/src/components/ui/MapSection/MapSection.js
+++ core/frontend/src/components/ui/MapSection/MapSection.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header } from './style/MapSectionElements'
+import { Header, MapDiv } from './style/MapSectionElements'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 const MapSection = () => {
@@ -9,17 +9,18 @@ const MapSection = () => {
             <div className='container py-5'>
                 <Header>Mapa Interactivo</Header>
                 <div className='row py-5'>
-                    {/* <MapContainer /> */}
-                    <MapContainer
-                        center={[-31.1388288,-64.4520141]} 
-                        zoom={13} 
-                        style={{ width: '100%', height: '500px'}}
-                    >
-                        <TileLayer
-                            attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                        />
-                    </MapContainer>
+                    {/* <MapDiv> */}
+                        <MapContainer
+                            center={[-31.1388288,-64.4520141]} 
+                            zoom={13} 
+                            style={{ width: '100%', height: '500px'}}
+                        >
+                            <TileLayer
+                                attribution='&copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                        </MapContainer>
+                    {/* </MapDiv> */}
                 </div>
             </div>
         </>
