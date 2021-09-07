a577dc1e53d843b3b56ff220621e3575b0089eef
ElUrquii <urquizamaxi099@gmail.com>
2021-09-07 10:02:37 -0300

    Finished cards on 'Colabora' section

---
+++ core/frontend/src/components/ui/MLibreInfoSection/Card/MLibreCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+import MLibreLogo from "../static/mercado-libre-logo.png";
+
+const MLCard = styled.div.attrs({
+  className:
+    "card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow",
+})`
+  background-image: linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.3)),
+    url(${MLibreLogo});
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  border-radius: 1rem;
+  border-color: white;
+  transition: transform 0.1s;
+  cursor: pointer;
+
+  &:hover {
+    background-image: linear-gradient(rgba(0, 0, 0, 0.06), rgba(0, 0, 0, 0.2)),
+      url(${MLibreLogo});
+    border-color: #50b559;
+    transform: scale(1.06);
+  }
+`;
+
+const Card = () => {
+  return (
+    <>
+      <MLCard>
+        <div className="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+          <h2 className="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">Card Title</h2>
+        </div>
+      </MLCard>
+    </>
+  );
+};
+
+export default Card;
--- core/frontend/src/components/ui/MLibreInfoSection/MLibreInfoSection.js
+++ core/frontend/src/components/ui/MLibreInfoSection/MLibreInfoSection.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Card from "./Card/MLibreCard";
+import axios from "axios";
 import styled from "styled-components";
 
+const Header = styled.h2.attrs({
+  className: "pb-2 border-bootom fw-bold",
+})`
+  color: rgb(62, 35, 7);
+`;
+
 const MLibreInfoSection = () => {
+  const [cardData, setCardData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/mercado-libre/")
+      .then((response) => {
+        setCardData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
-    <div>
-      <h1>Hola</h1>
-    </div>
+    <>
+      <div className="container py-5">
+        <Header>Nuestras tiendas</Header>
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+          {cardData.map((card) => {
+            return (
+              <div className="col">
+                <Card />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
   );
 };
 
