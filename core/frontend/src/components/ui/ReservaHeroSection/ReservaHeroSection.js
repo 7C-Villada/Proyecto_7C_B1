d1a261c73ba0406313dde7f229853d60f6fa8742
BenjaCimatti <benja.cimatti@gmail.com>
2021-10-12 11:45:00 -0300

    Form mail Reserva

--- core/frontend/src/components/ui/ReservaHeroSection/ReservaHeroSection.js
+++ core/frontend/src/components/ui/ReservaHeroSection/ReservaHeroSection.js
@@ -39,6 +39,21 @@ background-color: #f8f0bb;
 }
 `;
 
+export const SubmitButton = styled.button.attrs(props => ({
+  className:"shadow btn btn-floating btn-lg px-5 me-md-2",
+  href: props.href,
+}))`
+color: rgba(62,35,7,1) !important;
+background-color: #f8f0bb;
+&:hover {
+  background-color: rgba(62,35,7,1) !important;
+  color: #f8f0bb !important;
+}
+&:focus {
+  box-shadow: none !important;
+}
+`;
+
 export const buttonText = styled.p.attrs({
   className: "display-5 border-bottom fw-bold lh-1 mb-3",
 })`
@@ -60,11 +75,11 @@ const ReservaHeroSection = () => {
                       <span className="col align-middle small">Facebook</span>
                     </div>
                   </SocialMediaButton>
-                  <SocialMediaButton href={"mailto:cordobagilwellpark@gmail.com"} gmail>
+                  {/* <SocialMediaButton href={"mailto:cordobagilwellpark@gmail.com"} gmail>
                     <div className="row align-items-center gx-3">
                       <span className="col align-middle small">E-Mail</span>
                     </div>
-                  </SocialMediaButton>
+                  </SocialMediaButton> */}
                 </div>
               </div>
               <div className="col">
@@ -75,6 +90,22 @@ const ReservaHeroSection = () => {
                 </div>
               </div>
             </div>
+            {/* Hay q cambiar por su mail */}
+            <form action="https://formsubmit.co/6bae887b0aa2cd51eb857248d3b9154f" method="POST">
+              <div class="mb-3 mt-5">
+                <Header>Contactanos</Header>
+                <label for="exampleInputEmail1" class="form-label">Dirección Email</label>
+                <input type="email" name="email" class="form-control" id="exampleInputEmail1" required></input>
+              </div>
+              <div class="mb-3">
+                <input type="hidden" name="_subject" value="Contacto Reserva" class="form-control" id="exampleInputSubject1"></input>
+              </div>
+              <div class="mb-3">
+                <label for="exampleContent1" class="form-label">Contenido</label>
+                <textarea type="text" name="message" className="form-control" id="exampleContent1" rows="4" cols="50" required></textarea>
+              </div>
+              <SubmitButton type="submit">Enviar</SubmitButton>
+            </form>
           </div>
     </Background>
     </>
