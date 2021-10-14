5a7095de3085e122442afb9b5c8cf0310d784c3a
ElUrquii <urquizamaxi099@gmail.com>
2021-10-14 11:53:35 -0300

    Lazy Loading on both carousels in home page

--- core/frontend/src/components/views/Home/Home.js
+++ core/frontend/src/components/views/Home/Home.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 // UI modules
 import Hero from "../../ui/Hero/Hero";
 import CardSection from "../../ui/CardSection/CardSection";
-import CarouselSection from "../../ui/CarouselSection/CarouselSection";
+// import CarouselSection from "../../ui/CarouselSection/CarouselSection";
 import { Divider } from "../../ui/Divider/Divider";
-import ConveniosSection from "../../ui/ConveniosSection/ConveniosSection";
-import MapSection from '../../ui/MapSection/MapSection'
+// import ConveniosSection from "../../ui/ConveniosSection/ConveniosSection";
+import MapSection from "../../ui/MapSection/MapSection";
 
+const CarouselSection = React.lazy(() =>
+  import("../../ui/CarouselSection/CarouselSection")
+);
+
+const ConveniosSection = React.lazy(() =>
+  import("../../ui/ConveniosSection/ConveniosSection")
+);
 
 // Styles modules
 
@@ -18,12 +25,15 @@ const Home = () => {
       <Divider></Divider>
       <CardSection />
       <Divider></Divider>
-      <CarouselSection></CarouselSection>
+      <Suspense fallback={<div>Loading...</div>}>
+        <CarouselSection />
+      </Suspense>
       <Divider></Divider>
       {/* <MapSection />
       <Divider></Divider> */}
-      <ConveniosSection></ConveniosSection>
-
+      <Suspense fallback={<div>Loading...</div>}>
+        <ConveniosSection />
+      </Suspense>
     </>
   );
 };
