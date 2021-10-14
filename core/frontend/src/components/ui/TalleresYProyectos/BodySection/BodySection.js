fecb51aa75251916eccb69d2828e0be297ee7eb2
ElUrquii <urquizamaxi099@gmail.com>
2021-10-14 10:38:50 -0300

    Fixed 'talleres y proyectos' view bug when data arrays are empty

--- core/frontend/src/components/ui/TalleresYProyectos/BodySection/BodySection.js
+++ core/frontend/src/components/ui/TalleresYProyectos/BodySection/BodySection.js
@@ -1,77 +1,89 @@
 import React, { useState, useEffect } from "react";
-import {
-  BodyContainer,
-  CardContainer,
-  CardCol,
-
-  Square,
-
-} from "./BodyElements";
+import { BodyContainer, CardContainer, CardCol, Square } from "./BodyElements";
 import Card from "./Card";
-import axios from 'axios';
+import axios from "axios";
 
 const BodySection = () => {
-
-  const [talleres, setTalleres] = useState([])
-
-  useEffect(() => {
-    axios
-      .get('/api/taller/')
-      .then((response) => {
-        setTalleres(response.data)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }, [])
-
-  const [proyectos, setProyectos] = useState([])
+  const [talleres, setTalleres] = useState([]);
 
   useEffect(() => {
     axios
-      .get('/api/proyecto/')
+      .get("/api/taller/")
       .then((response) => {
-        setProyectos(response.data)
+        setTalleres(response.data);
       })
       .catch((error) => {
-        console.log(error)
+        console.log(error);
+      });
+  }, []);
+
+  const [proyectos, setProyectos] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("/api/proyecto/")
+      .then((response) => {
+        setProyectos(response.data);
       })
-  }, [])
-
-
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   return (
     <BodyContainer>
       <CardContainer>
+        {talleres != [] ? (
+          talleres
+            .filter((taller) => taller.title.includes(""))
+            .map((filteredTaller) => {
+              return (
+                <CardCol>
+                  <Square>
+                    <Card
+                      url={"taller/"}
+                      id={filteredTaller.id}
+                      titulo={filteredTaller.title}
+                      sDate={filteredTaller.startDate}
+                      image={filteredTaller.imagen}
+                    />
+                  </Square>
+                </CardCol>
+              );
+            })
+        ) : (
+          <div className="col pt-3">
+            <p className="small text-muted">
+              <em>Ups... Parece que aún no hay nadie.</em>
+            </p>
+          </div>
+        )}
 
-        {talleres.filter(taller => taller.title.includes("")).map((filteredTaller) => {
-          return (
-
-            <CardCol>
-              <Square>
-                
-                <Card url={"taller/"} id={filteredTaller.id} titulo={filteredTaller.title} sDate={filteredTaller.startDate} image={filteredTaller.imagen} />
-
-              </Square>
-            </CardCol>
-
-          )
-        })}
-
-        {proyectos.filter(proyecto => proyecto.title.includes("")).map((filteredProyecto) => {
-          return (
-
-            <CardCol>
-              <Square>
-                
-                <Card url={"proyecto/"} id={filteredProyecto.id} titulo={filteredProyecto.title} sDate={filteredProyecto.startDate} image={filteredProyecto.imagen} />
-
-              </Square>
-            </CardCol>
-
-          )
-        })}
-
+        {proyectos != [] ? (
+          proyectos
+            .filter((proyecto) => proyecto.title.includes(""))
+            .map((filteredProyecto) => {
+              return (
+                <CardCol>
+                  <Square>
+                    <Card
+                      url={"proyecto/"}
+                      id={filteredProyecto.id}
+                      titulo={filteredProyecto.title}
+                      sDate={filteredProyecto.startDate}
+                      image={filteredProyecto.imagen}
+                    />
+                  </Square>
+                </CardCol>
+              );
+            })
+        ) : (
+          <div className="col pt-3">
+            <p className="small text-muted">
+              <em>Ups... Parece que aún no hay nadie.</em>
+            </p>
+          </div>
+        )}
       </CardContainer>
     </BodyContainer>
   );
