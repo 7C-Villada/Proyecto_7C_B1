7ba8e87d3fb363b2a01899d24fc76b3178473e1e
ElUrquii <urquizamaxi099@gmail.com>
2021-11-12 10:10:03 -0300

    Revert "View "talleres y proyectos" modificada, search bar andando"
    This reverts commit d6dac8cbfdb823957b9e0e82ec85ab6ce2b61074.

---
+++ core/frontend/src/components/views/TalleresProyectos/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import FilterSection from "../../ui/TalleresProyectos/FilterSection";
+
+const TalleresProyectos = () => {
+  return (
+    <>
+      <FilterSection />
+    </>
+  );
+};
+
+export default TalleresProyectos;
