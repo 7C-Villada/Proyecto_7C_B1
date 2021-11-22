b1026e1d180a06e5b72844b024d0e549c75301e2
ElUrquii <urquizamaxi099@gmail.com>
2021-11-22 19:37:41 -0300

    Changed typ input style

--- core/frontend/src/components/ui/TalleresProyectos/FilterSectionElements.js
+++ core/frontend/src/components/ui/TalleresProyectos/FilterSectionElements.js
@@ -1,10 +1,8 @@
 import styled from "styled-components";
 
 export const FilterContainer = styled.div.attrs({
-  className: "d-flex flex-column flex-sm-row py-3 px-3 rounded",
-})`
-  background-color: #e5e4e2;
-`;
+  className: "d-flex flex-column flex-sm-row py-3 px-3",
+})``;
 
 export const SearchInput = styled.input.attrs({
   type: "text",
@@ -12,6 +10,16 @@ export const SearchInput = styled.input.attrs({
   placeholder: "Buscar",
 })`
   border-radius: 75px 0 0 75px;
+  background-color: #e5e4e2;
+
+  :focus {
+    background-color: #e5e4e2;
+    color: rgb(62, 35, 7);
+  }
+
+  ::placeholder {
+    color: rgb(62, 35, 7);
+  }
 
   @media screen and (max-width: 576px) {
     border-radius: 10px 10px 0 0;
